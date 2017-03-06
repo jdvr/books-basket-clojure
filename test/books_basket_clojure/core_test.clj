b1547bef19d62387c25147101f70aa34d7dabda4
jdvega <jdvega@idealista.com>
2017-03-06 09:10:50 +0100

    red:given a basket with three IT books should return the sum of every book price after applying a 30% to each price

--- test/books_basket_clojure/core_test.clj
+++ test/books_basket_clojure/core_test.clj
@@ -4,6 +4,7 @@
 
 (def not-eligible-for-discount [{:theme :Beauty :price 5} {:theme :Education :price 7} {:theme :Love :price 9}])
 (def all-eligible-for-discount [{:theme :Fantasy :price 10} {:theme :IT :price 10}])
+(def it-books-for-discount [{:theme :IT :price 10} {:theme :IT :price 20} {:theme :IT :price 30}])
 
 (deftest gross-of-a-books-basket
   (testing "given an empty basket should return zero"
@@ -27,6 +28,10 @@
 
   (testing "given a basket full of book eligible for a discount should return the sum of every book price after applying the discount"
     (is (= 17.0 (net all-eligible-for-discount)))
+    )
+
+  (testing "given a basket with three IT books should return the sum of every book price after applying a 30% to each price"
+    (is (= 42 (net it-books-for-discount)))
     )
 
 
