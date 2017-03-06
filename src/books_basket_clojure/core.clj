68a9fca867dcf8a1f5bc31e55e63647aeffc14be
jdvega <jdvega@idealista.com>
2017-03-06 08:39:19 +0100

    red: given a basket full of boot not eligible for a discount should return the sum of every book price

--- src/books_basket_clojure/core.clj
+++ src/books_basket_clojure/core.clj
@@ -1,7 +1,7 @@
 (ns books-basket-clojure.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+
+(defn total [books]
+  0)
--- test/books_basket_clojure/core_test.clj
+++ test/books_basket_clojure/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [books-basket-clojure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def not-eligible-for-discount [{:theme :Beauty :price 5} {:theme :Education :price 7} {:theme :Love :price 9}])
+
+(deftest total-of-a-books-basket
+  (testing "given an empty basket should return zero"
+    (is (= 0 (total [])))
+    )
+
+  (testing "given a basket full of boot not eligible for a discount should return the sum of every book price"
+    (is (= 21 (total not-eligible-for-discount)))
+    )
+  )
