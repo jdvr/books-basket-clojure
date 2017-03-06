700ffcbd4a8e92976f1a3ae318e8ba86765223a8
jdvega <jdvega@idealista.com>
2017-03-06 08:54:19 +0100

    red: given a basket full of book eligible for a discount should return the sum of every book price after applying the discount

--- test/books_basket_clojure/core_test.clj
+++ test/books_basket_clojure/core_test.clj
@@ -3,19 +3,19 @@
             [books-basket-clojure.core :refer :all]))
 
 (def not-eligible-for-discount [{:theme :Beauty :price 5} {:theme :Education :price 7} {:theme :Love :price 9}])
-(def eligible-for-discount [{:theme :Fantasy :price 5} {:theme :IT :price 7} {:theme :Travel :price 9}])
+(def all-eligible-for-discount [{:theme :Fantasy :price 10} {:theme :IT :price 10}])
 
 (deftest gross-of-a-books-basket
   (testing "given an empty basket should return zero"
     (is (= 0 (gross [])))
     )
 
-  (testing "given a basket full of boot not eligible for a discount should return the sum of every book price"
+  (testing "given a basket full of book not eligible for a discount should return the sum of every book price"
     (is (= 21 (gross not-eligible-for-discount)))
     )
 
-  (testing "given a basket full of boot with a discount should return the sum of every book price"
-    (is (= 21 (gross eligible-for-discount)))
+  (testing "given a basket full of book with a discount should return the sum of every book price"
+    (is (= 21 (gross all-eligible-for-discount)))
     )
   )
 
@@ -24,6 +24,17 @@
   (testing "given an empty basket should return zero"
     (is (= 0 (net [])))
     )
+
+  (testing "given a basket full of book eligible for a discount should return the sum of every book price"
+    (is (= 21 (net not-eligible-for-discount)))
+    )
+
+
+  (testing "given a basket full of book eligible for a discount should return the sum of every book price"
+    (is (= 17 (net all-eligible-for-discount)))
+    )
+
+
   )
 
 
